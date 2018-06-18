c2982111d0164897a1765beb05761f3d4d051e72
kachamaka <martin.popov42@gmail.com>
2018-06-18 08:06:26 +0300

    fix

--- postData.js
+++ postData.js
@@ -4,6 +4,7 @@ function sendToPHP(){
         email: document.getElementById("email").value,
         password: document.getElementById("password").value
     }
+    
     $.post( "getAndSaveData.php", userCredentials,function(data){
             $("#myDiv").html(data);
         }
